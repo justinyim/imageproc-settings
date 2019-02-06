65e80835fffd1570a70fcd4f46aa529c6075e474
justinyim <justin.ks.yim@gmail.com>
2019-02-06 15:53:40 -0800

    Up to date Rudolph & Dasher parameters. Adding FULL_POWER and BLDC_CMD_OFFSET.

--- settings.h
+++ settings.h
@@ -27,25 +27,29 @@
 #define SALTO_1P_RUDOLPH 2
 #define SALTO_1P_DASHER 3
 
-#define ROBOT_NAME SALTO_1P_DASHER
+#define ROBOT_NAME SALTO_1P_DASHER // SALTO_1P_RUDOLPH // 
 
 // Robot-specific parameters (like encoder offsets)
 #if ROBOT_NAME == SALTO_1P_RUDOLPH // Rudolph
 // Encoder offsets
 #define AMS_ENC_OFFSET_0        0 // tail encoder
-#define AMS_ENC_OFFSET_1        10545 // femur encoder
-								// pre-reguling: 17445
+#define AMS_ENC_OFFSET_1        15792 // femur encoder
+								// pre-regluing: 17445
 
-#define BLDC_MOTOR_OFFSET       1500 // motor offset from zero position
-#define BLDC_CALIB              2240 // AMS encoder commutation offset
+#define BLDC_MOTOR_OFFSET       4500 // motor offset from zero position
+#define BLDC_CALIB              2583 // recalibrate after magnet dislodged
+                                // 2240 // AMS encoder commutation offset
+#define BLDC_CMD_OFFSET         (1*65536) // leg motor command shift
 
-#define FULL_EXTENSION          11700 // motor radians at full extension
+#define FULL_EXTENSION          12500 // motor radians at full extension
 
 // Physical parameters
 #define FULL_MASS               26 // 103g, 2^8 ticks/kg
 #define LEG_FRICTION            130 // 0.13 Nm/Nm coefficient
 #define SPRING_LINEAR           0.3836493898315605 // Nm/rad
 #define SPRING_QUADRATIC        0.029279712493158 // Nm/rad^2
+
+//#define FULL_POWER
 
 #elif ROBOT_NAME == SALTO_1P_SANTA // Santa
 // Encoder offsets
@@ -58,6 +62,7 @@
 
 #define BLDC_MOTOR_OFFSET       2500
 #define BLDC_CALIB              1060 // AMS encoder commutation offset
+#define BLDC_CMD_OFFSET         0 // leg motor command shift
 
 #define FULL_EXTENSION          14500 // motor radians at full extension
 
@@ -67,23 +72,32 @@
 #define SPRING_LINEAR           0.3836493898315605 // Nm/rad
 #define SPRING_QUADRATIC        0.029279712493158 // Nm/rad^2
 
+#define FULL_POWER
+
 #elif ROBOT_NAME == SALTO_1P_DASHER // Dasher
 // Encoder offsets
 #define AMS_ENC_OFFSET_0        0 // tail encoder
-#define AMS_ENC_OFFSET_1        17668 // 18101-433 = 17668. femur encoder (offset in rad)/(2*pi)*2^14
+#define AMS_ENC_OFFSET_1        10003 // -20 on 9/11, -57 on 9/10, -50 on 8/28, -65 re-build crank 8/21
+                                // 10195 // reset femur 8/16/2018
+                                //femur slip: -160,-100,-230,-104,-130 on 8/15, -130 on 8/14, -150 on 8/13
+                                //9750 // rebuilt and reset 8/6/2018
+                                //17668 // 18101-433 = 17668. femur encoder (offset in rad)/(2*pi)*2^14
 								//18101 (pre 6/25/18) //17921 //19094 // femur encoder
 
-#define BLDC_MOTOR_OFFSET       1000 // motor offset from zero position
+#define BLDC_MOTOR_OFFSET       3500 // motor offset from zero position
 								// 3000 (pre 6/25/18) 
 #define BLDC_CALIB              135//2080 // AMS encoder commutation offset
+#define BLDC_CMD_OFFSET         0 // leg motor command shift
 
-#define FULL_EXTENSION          13400 // motor radians at full extension
+#define FULL_EXTENSION          12400 // motor radians at full extension (for femur)
 
 // Physical parameters
-#define FULL_MASS               26 // 103g, 2^8 ticks/kg
+#define FULL_MASS               28 // 108.0g, 2^8 ticks/kg with shell (previously 105.0g)
 #define LEG_FRICTION            180 // 0.18 Nm/Nm coefficient
 #define SPRING_LINEAR           0.3759274976416161 // Nm/rad
 #define SPRING_QUADRATIC        0.04275050419811049 // Nm/rad^2
+
+#define FULL_POWER
 
 #endif // ROBOT_NAME
 
